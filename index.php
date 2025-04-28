84435e060678ca566a35506c546905a7d48fee24
oscar5677 <72199362+oscar5677@users.noreply.github.com>
2025-04-29 00:02:41 +0530

    Update index.php

--- index.php
+++ index.php
@@ -17,7 +17,7 @@ $mpdUrl = 'https://ucdn.starhubgo.com/' . $get;
 
 $mpdheads = [
     'http' => [
-      //'header' => "User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:109.0) Gecko/20100101 Firefox/113.0",
+     'header' => "User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:109.0) Gecko/20100101 Firefox/113.0\r\n"
      //'header' =>  "User-Agent: user-agent=user-agent=Mozilla/5.0 (Linux; Android 10; MI 9 Build/QKQ1.190825.002; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/111.0.5563.58 Mobile Safari/537.36\r\n",
  
         'follow_location' => 1,
