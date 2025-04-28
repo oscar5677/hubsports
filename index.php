89fc7ad2c206d1eb989d33eb2b7003426af68211
oscar5677 <72199362+oscar5677@users.noreply.github.com>
2025-04-28 23:38:11 +0530

    Update index.php

--- index.php
+++ index.php
@@ -33,10 +33,8 @@ if ($res === false) {
     exit();
 }
 
-header("Content-Type: application/dash+xml");
-header("Cache-Control: no-cache, no-store, must-revalidate");
-header("Pragma: no-cache");
-header("Expires: 0");
+
+
 
 echo $res;
 ?>
